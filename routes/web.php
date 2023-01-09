15924f119650c990d1369f8dd5e607bed31517ca
LastWord7722 <LastWordMark@gmail.com>
2023-01-09 12:59:32 +0200

    editing sample, separated header and footer

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/main', [MainController::class, 'index']);
+Route::get('/main', [MainController::class, 'index'])->name('main');
