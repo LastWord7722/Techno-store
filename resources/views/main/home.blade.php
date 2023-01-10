695eb1bdb3962ddbb02a1eeb8f8e51746472c2c8
LastWord7722 <LastWordMark@gmail.com>
2023-01-10 18:47:15 +0200

    editing html

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -2,7 +2,7 @@
 @section('Content')
     <main>
         <!--категории?-->
-        <div class="catagory4 pt-60 pb-60">
+        <div class="catagory4 pt-60 pb-60 mt-5">
 
             <p>Первый роут работает, шаблон тоже </p>
             <p>Первый роут работает, шаблон тоже </p>
