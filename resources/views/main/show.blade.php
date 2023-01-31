cd03ca1ae80fed22060a3cea850ea942b8cf98b7
LastWord7722 <LastWordMark@gmail.com>
2023-01-31 11:24:46 +0200

    low correct view

--- resources/views/main/show.blade.php
+++ resources/views/main/show.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.layouts')
 @section('Content')
     <section class="products-grid pt-60 pb-60 mt-5 row">
-        <div class=" col-4">
-            <img class="card-img-top" src="{{url('storage/' . $product->image)}}" alt="нема" style="width: 18rem;">
 
+        <div class="col-md-5 g-lg-5 ">
+            <img class="card-img-top" src="{{url('storage/' . $product->image)}}" alt="нема" style="width: 18rem; margin-left: 100px;">
         </div>
 
-        <div class="col-8">
+        <div class="col-md-7">
             <ul class="list-group list-group-flush col-5 text-center">
                 <li class="list-group-item">Бренд :{{$product->brand->title}}</li>
                 <li class="list-group-item">Категория :{{$product->category->title}}</li>
@@ -17,9 +17,18 @@
                 </li>
             </ul>
         </div>
-        <div class="col-3">
-        <button class=" mt-5 btn btn-danger"> В корзину</button>
+        <div class="col-3 d-flex flex-row">
+            <form action="{{route('addCard',$product->id)}}" method="post">
+                @csrf @method('POST')
+                <button type="submit" class="btn btn-outline-dark "> <i class="fa-solid fa-basket-shopping"> </i></button>
+            </form>
+
         <a href="{{route('main.index')}}" class="btn-primary mt-5 btn btn-primary">Назад</a>
+        </div>
+        {{--секция комментов--}}
+
+        <div>
+            
         </div>
     </section>
 @endsection
